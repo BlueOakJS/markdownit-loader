44a3f349bfcfe1e3602819697a65ca9de028b5c4
Andre Asselin <andre.asselin@pointsource.com>
2017-03-06 15:55:38 -0500

    Add example of using markdown from an external file

--- example/src/app.vue
+++ example/src/app.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="markdown-body">
     <markdown></markdown>
+    <div v-html="md"></div>
   </div>
 </template>
 
 <script>
 import markdown from './markdown.vue'
+import markdown2 from './markdown2.md'
 import 'highlight.js/styles/github.css'
 import 'github-markdown-css'
 
 export default {
   components: {
     markdown
+  },
+  data: function() {
+    return {
+      md: markdown2
+    }
   }
 }
 </script>
